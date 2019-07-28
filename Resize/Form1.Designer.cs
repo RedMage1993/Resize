001f4d7af720010efab33e3b94b5137d2fb6891e
Fritz Ammon <redmage1993@gmail.com>
2019-07-27 22:56:57 -0700

    initial commit; had already set up ui

---
+++ Resize/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Resize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.comboBoxWindow = new System.Windows.Forms.ComboBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonResize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Window";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxWidth.Location = new System.Drawing.Point(124, 25);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(59, 20);
+            this.textBoxWidth.TabIndex = 3;
+
+            // 
+            // comboBoxWindow
+            // 
+            this.comboBoxWindow.FormattingEnabled = true;
+            this.comboBoxWindow.Location = new System.Drawing.Point(12, 24);
+            this.comboBoxWindow.Name = "comboBoxWindow";
+            this.comboBoxWindow.Size = new System.Drawing.Size(103, 21);
+            this.comboBoxWindow.TabIndex = 4;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxHeight.Location = new System.Drawing.Point(189, 25);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(59, 20);
+            this.textBoxHeight.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Height";
+            // 
+            // buttonResize
+            // 
+            this.buttonResize.Location = new System.Drawing.Point(124, 51);
+            this.buttonResize.Name = "buttonResize";
+            this.buttonResize.Size = new System.Drawing.Size(124, 29);
+            this.buttonResize.TabIndex = 7;
+            this.buttonResize.Text = "Resize";
+            this.buttonResize.UseVisualStyleBackColor = true;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 92);
+            this.Controls.Add(this.buttonResize);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxWindow);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Resize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.ComboBox comboBoxWindow;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonResize;
+    }
+}
+
